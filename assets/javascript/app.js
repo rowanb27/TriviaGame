8e35d43b64b0db81304fed7ac490c58f7e39d0fa
Robert Rowan <rowanb27@yahoo.com>
2019-03-23 17:27:39 -0500

    updated javascript questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,9 +7,33 @@ $(document).ready(function() {
           question: "What would be the value of x in the equation 2x + 5 = 11?",
           choice: ["3", "1/2", "6", "2/11"],
           answer: 0,
-          gif: "assets\images\3.webp",
-          photo: "assets\images\3.JPG"
-      }
+          correctImage: "assets\images\3.webp",
+          incorrectImage: "assets\images\3.JPG"
+      },
+
+      {
+          question: "assets\images\limit.jpg", 
+          choice: ["-.455", "I give up", "0", "The limit does not exist"],
+          answer: 3,
+          correctImage: "assets\images\limitGif.webp",
+          incorrectImage: "assets\images\limitGif.webp"
+      },
+
+      {
+          question: "The Fibonacci Sequence is the series of numbers: 0, 1, 1, 2, 3, 5,... What is the next number in the sequence?",
+          choice: ["10", "5", "8", "Math is hard"],
+          answer: 2,
+          correctImage: "assets\images\fibGif.webp",
+          incorrectImage: "assets\images\fibonacci.jpg"
+      },  
+
+      {
+          question: "What would be the value of x in the equation 2+2×2−2×2?",
+          choice: ["2", "-2", "4", "8"],
+          answer: 0,
+          correctImage: "assets\images\orderOfOpsGif.webp",
+          incorrectImage: "assets\images\orderOfOperations.JPG"
+      },  
   ]
   console.log(options);
 
