e17757c0fd0d2ab8c5e3be90be1ab4c51d832498
Robert Rowan <rowanb27@yahoo.com>
2019-03-23 16:54:46 -0500

    updating javascript

---
+++ assets/javascript/app.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+  
+  // Created a variable and set variable to an array of objects containing the following: question, choices, answer, 2 images(1 gif, 1 solved answer)
+
+  var options = [
+      {
+          question: "What would be the value of x in the equation 2x + 5 = 11?",
+          choice: ["3", "1/2", "6", "2/11"],
+          answer: 0,
+          gif: "assets\images\3.webp",
+          photo: "assets\images\3.JPG"
+      }
+  ]
+  console.log(options);
+
+
+
+
+
+
+
+
+
+
+
+
+})
